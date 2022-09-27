f31fb753209eeab2e00e2ca8cd0833a06e74242b
Bruno Paveglio <pavegliobruno@gmail.com>
2022-09-27 08:37:33 +0200

    Updating CV witbh 2022 version

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -14,8 +14,8 @@ export const Contact = () => {
 		linkedin: 'https://www.linkedin.com/in/pavegliobruno/',
 		github: 'https://github.com/Pavegliobruno',
 		cv: {
-			es: 'https://drive.google.com/file/d/1righLhxb_MX11vslZmvJagYlXOEc3UM2/view?usp=sharing',
-			en: 'https://drive.google.com/file/d/1righLhxb_MX11vslZmvJagYlXOEc3UM2/view?usp=sharing',
+			es: 'https://drive.google.com/file/d/11qDxenYfjUlorWzTBWqJTCqOrEuB3q1i/view?usp=sharing',
+			en: 'https://drive.google.com/file/d/1psgngp6vS0POc8rwKYJUzGEEE1GOSLc8/view?usp=sharing',
 		},
 	};
 	return (
@@ -48,7 +48,9 @@ export const Contact = () => {
 						<a
 							className='icon'
 							href={aboutMe.cv.es}
-							download='CV Paveglio Bruno - Full Stack'
+							download='/src/utils/PaveglioBrunoCv'
+							target='_blank'
+							rel='noopener noreferrer'
 						>
 							<RiPagesLine className='icon' key={'cv es'} /> {cv.es}
 						</a>
@@ -56,7 +58,9 @@ export const Contact = () => {
 						<a
 							className='icon'
 							href={aboutMe.cv.en}
-							download='CV Paveglio Bruno - Full Stack'
+							download='/src/utils/PaveglioBrunoCv'
+							target='_blank'
+							rel='noopener noreferrer'
 						>
 							<RiPagesLine className='icon' key={'cv en'} />
 						</a>
